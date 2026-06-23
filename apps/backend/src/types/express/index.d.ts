a6bb0c4b263e9ef01f5d182cb3ca3a93c1e27344
Mayur <svmayurkaarthic@gmail.com>
2026-06-24 01:10:06 +0530

    commit

---
+++ apps/backend/src/types/express/index.d.ts
@@ -0,0 +1,11 @@
+import "express";
+import { Multer } from "multer";
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Multer.File;
+      files?: Multer.File[] | { [fieldname: string]: Multer.File[] };
+    }
+  }
+}
